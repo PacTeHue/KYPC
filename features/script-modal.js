20cd3d5afa895dccbdb747c1a478678d520420f7
PacTeHue <vladnekiforov8@gmail.com>
2025-04-26 13:00:49 +0200

    26.04.2025

--- features/script-modal.js
+++ features/script-modal.js
@@ -1,4 +1,41 @@
+const loginModal = document.getElementById("loginModal");
+const registerModal = document.getElementById("registerModal");
+const loginBtn = document.getElementById("loginBtn");
+const registerBtn = document.getElementById("registerBtn");
+const closeLogin = document.getElementById("closeLogin");
+const closeRegister = document.getElementById("closeRegister");
 
+// Открытие модального окна для входа
+loginBtn.onclick = function() {
+    loginModal.style.display = "block";
+}
+
+// Открытие модального окна для регистрации
+registerBtn.onclick = function() {
+    registerModal.style.display = "block";
+}
+
+// Закрытие модального окна для входа
+closeLogin.onclick = function() {
+    loginModal.style.display = "none";
+}
+
+// Закрытие модального окна для регистрации
+closeRegister.onclick = function() {
+    registerModal.style.display = "none";
+}
+
+// Закрытие модальных окон при клике вне их содержимого
+window.onclick = function(event) {
+    if (event.target === loginModal) {
+        loginModal.style.display = "none";
+    }
+    if (event.target === registerModal) {
+        registerModal.style.display = "none";
+    }
+}
+
+// ------------------------------------------------------------------------------
     const items = document.querySelectorAll('.list-group-item');
     
     items.forEach(item => {
@@ -28,3 +65,4 @@
             }
         });
     };
+    // --------------------------------------------------------------------------
